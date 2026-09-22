e8cb43e07c30ef7e0e4f36bc2f56a97933591f9e
aitherium-sync <sync@aitherium.com>
2026-09-22 11:47:30 +0000

    sync: awkno 0.2.3 from monorepo

--- awkno/__init__.py
+++ awkno/__init__.py
@@ -15,7 +15,7 @@ from __future__ import annotations
 
 from awkno.corpus import AwknoPage, AwknoRegistry, NotFoundError
 
-__version__ = "0.2.1"
+__version__ = "0.2.3"
 
 __all__ = [
     "AwknoRegistry",
